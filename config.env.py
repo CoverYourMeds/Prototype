34c9907110a05b11c71e574a8fd27b598a07c719
Devin Matte <devinmatte@gmail.com>
2018-03-19 13:46:50 -0400

    Default IP and Lock

--- config.env.py
+++ config.env.py
@@ -2,8 +2,8 @@
 import os
 
 DEBUG = True
-IP = 'localhost'
-PORT = '3000'
-SERVER_NAME = 'localhost:3000'
+IP = '0.0.0.0'
+PORT = '8080'
+SERVER_NAME = '0.0.0.0:8080'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db"))
