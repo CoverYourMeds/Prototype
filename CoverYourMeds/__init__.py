1d28b5cfbc5908533bcd13fd667ba531cea29063
Devin Matte <devinmatte@gmail.com>
2018-03-29 16:10:29 -0400

    Adding full front page support

--- CoverYourMeds/__init__.py
+++ CoverYourMeds/__init__.py
@@ -16,13 +16,16 @@ else:
 db = SQLAlchemy(app)
 
 # Import Model after DB set
-from CoverYourMeds.models import Medication
+from CoverYourMeds.models import Medication, Times
 
 
 @app.route("/", methods=["GET"])
 def home():
     db.create_all()
-    return render_template("index.html")
+
+    times = Times.query.all()
+    schedule = make_schedule(times)
+    return render_template("index.html", schedule=schedule)
 
 
 @app.route("/mymeds", methods=["GET"])
@@ -37,3 +40,44 @@ def mymeds():
 @app.route("/settings", methods=["GET"])
 def settings():
     return render_template("settings.html")
+
+
+def make_schedule(times):
+    schedule = {"sun": {}, "mon": {}, "tues": {}, "wed": {}, "thur": {}, "fri": {}, "sat": {}}
+    for time in times:
+        if time.sun:
+            if time.time in schedule["sun"]:
+                schedule["sun"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["sun"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+        if time.mon:
+            if time.time in schedule["mon"]:
+                schedule["mon"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["mon"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+        if time.tues:
+            if time.time in schedule["tues"]:
+                schedule["tues"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["tues"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+        if time.wed:
+            if time.time in schedule["wed"]:
+                schedule["wed"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["wed"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+        if time.thur:
+            if time.time in schedule["thur"]:
+                schedule["thur"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["thur"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+        if time.fri:
+            if time.time in schedule["fri"]:
+                schedule["fri"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["fri"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+        if time.sat:
+            if time.time in schedule["sat"]:
+                schedule["sat"][time.time].append(Medication.query.filter_by(med_id=time.medication).first())
+            else:
+                schedule["sat"][time.time] = [Medication.query.filter_by(med_id=time.medication).first()]
+    return schedule
--- CoverYourMeds/models.py
+++ CoverYourMeds/models.py
@@ -1,7 +1,28 @@
 from CoverYourMeds import db
 
 
+class User(db.Model):
+    __tablename__ = "users"
+    username = db.Column(db.VARCHAR(50), primary_key=True, nullable=False, unique=True)
+    password = db.Column(db.VARCHAR(50), nullable=False)
+
+
 class Medication(db.Model):
     __tablename__ = "meds"
     med_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
     name = db.Column(db.VARCHAR(150), nullable=False)
+    pills = db.Column(db.Integer)
+
+
+class Times(db.Model):
+    __tablename__ = "times"
+    time_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True, unique=True)
+    time = db.Column(db.Integer)
+    medication = db.Column(db.Integer, db.ForeignKey("meds.med_id"), nullable=False)
+    sun = db.Column(db.Boolean)
+    mon = db.Column(db.Boolean)
+    tues = db.Column(db.Boolean)
+    wed = db.Column(db.Boolean)
+    thur = db.Column(db.Boolean)
+    fri = db.Column(db.Boolean)
+    sat = db.Column(db.Boolean)
