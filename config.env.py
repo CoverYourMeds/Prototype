395a0a30dd309312debc2279016404c78a11299b
Roger <rmp7753@rit.edu>
2018-04-22 15:22:29 -0400

    Fixed Config

--- config.env.py
+++ config.env.py
@@ -4,8 +4,8 @@ from os import environ as env
 
 # Flask config
 DEBUG = True
-IP = env.get('IP', 'localhost')
+IP = env.get('IP', '0.0.0.0')
 PORT = env.get('PORT', 8080)
-SERVER_NAME = env.get('SERVER_NAME', 'localhost:8080')
+SERVER_NAME = env.get('SERVER_NAME', '0.0.0.0:8080')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db"))
