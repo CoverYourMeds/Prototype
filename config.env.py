e3b505e583192a894c2af2d2d85ea02dba88342d
Roger <rmp7753@rit.edu>
2018-04-22 15:17:50 -0400

    Small change to doctor view

--- config.env.py
+++ config.env.py
@@ -4,8 +4,8 @@ from os import environ as env
 
 # Flask config
 DEBUG = True
-IP = env.get('IP', '0.0.0.0')
+IP = env.get('IP', 'localhost')
 PORT = env.get('PORT', 8080)
-SERVER_NAME = env.get('SERVER_NAME', '0.0.0.0:8080')
+SERVER_NAME = env.get('SERVER_NAME', 'localhost:8080')
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(os.getcwd(), "data.db"))
